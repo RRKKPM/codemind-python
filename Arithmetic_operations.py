5be68e73763805935c1524a0a8c96ac393fab381
RRKKPM <120611519+RRKKPM@users.noreply.github.com>
2022-12-30 11:54:06 +0530

    a=a=int(input()) b=int(input()) print("Sum:{}".format(a+b)) print("Difference:{}".format(a-b)) print("Product:{}".format(a*b)) print("Quotient:{}".format(a//b)) print("Remainder:{}".format(a%b))

---
+++ Arithmetic_operations.py
@@ -0,0 +1,8 @@
+a=a=int(input())
+b=int(input())
+print("Sum:{}".format(a+b))
+print("Difference:{}".format(a-b))
+print("Product:{}".format(a*b))
+print("Quotient:{}".format(a//b))
+print("Remainder:{}".format(a%b))
+
