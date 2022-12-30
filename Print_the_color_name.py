70dac1d7dcaa77983303ef34630bf921dd770bbe
RRKKPM <120611519+RRKKPM@users.noreply.github.com>
2022-12-30 22:31:22 +0530

    col=input() if col=='V':     print("Violet") elif col=='I':     print("Indigo") elif col=='B':     print("Blue") elif col=='G':     print("Green") elif col=='Y':     print("Yellow") elif col=='O':     print("Orange") elif col=='R':     print("Red") else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+col=input()
+if col=='V':
+    print("Violet")
+elif col=='I':
+    print("Indigo")
+elif col=='B':
+    print("Blue")
+elif col=='G':
+    print("Green")
+elif col=='Y':
+    print("Yellow")
+elif col=='O':
+    print("Orange")
+elif col=='R':
+    print("Red")
+else:
+    print(-1)
